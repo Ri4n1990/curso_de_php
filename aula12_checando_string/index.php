5b5dff79b4c77c4e8fbad92715f792219b06e19a
Magnus <rianmagnus5791@gmail.com>
2025-10-25 09:06:10 -0300

    checando string aula12 e exercicio 05

---
+++ aula12_checando_string/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$texto = "minha string";
+
+if(is_string($texto)){
+    
+    echo "é uma string" ;
+
+}else{
+
+    echo "não é uma string";
+}
---
+++ exercicios/exercicio_05.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "duplas <br>";
+echo 'simples <br>' ;
+
+$nome = "matheus" ;
+$idade = 29 ;
+
+print "o nome do rapaz é $nome  e ele tem $idade anos.";
