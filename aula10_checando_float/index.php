d45a0b158e4f48ed2cee4eda94253227502cfbfd
Magnus <rianmagnus5791@gmail.com>
2025-10-25 08:49:21 -0300

    checando float aula10 e exercicio 04

---
+++ aula10_checando_float/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$floflo = 1.3;
+
+if(is_float($floflo)){
+    echo "é float a bomba";
+
+}else{
+    echo "não é float a bomba ";
+}
+ 
---
+++ exercicios/exercicio_04.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo  1.50 , "<br>" , 2.50, "<br>";
+
+$num = 1.50;
+if(is_float($num)){
+    echo $num ;
+}else{
+    echo "esse não é float";
+}
