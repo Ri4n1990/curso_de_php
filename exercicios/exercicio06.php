4c4bbd37832e83580516f19a28a47624d1e5dedd
Magnus <rianmagnus5791@gmail.com>
2025-10-25 09:39:47 -0300

    exercicio sobre arrays

---
+++ exercicios/exercicio06.php
@@ -0,0 +1,9 @@
+<?php
+
+$carro = ['rodas' , 'portas'] ;
+
+$caracteristicas = ['carac1' => 'porta-malas' , 'carac2' => 'porta-luvas' ];
+
+echo  $carro[0] , " ", $carro[1], "<br>";
+
+echo $caracteristicas['carac1'], " ", $caracteristicas['carac2'];
