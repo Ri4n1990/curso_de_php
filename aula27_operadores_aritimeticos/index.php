bb52da378e5ead51344c076d634f3fc3a086eb9f
Magnus <rianmagnus5791@gmail.com>
2025-10-27 09:51:36 -0300

    operadores aritimeticos aula 27 e exercicio 16

---
+++ aula27_operadores_aritimeticos/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$soma = 5 + 5 ;
+$subtracao = 5 - 5 ;
+$multiplicacao = 5 * 5 ;
+$divisao = 5 / 5 ;
+echo $soma  . "<br>" ;
+echo $multiplicacao  . "<br>" ;
+echo $divisao  . "<br>" ;
+echo $subtracao  ;
+
---
+++ exercicios/exercicio16.php
@@ -0,0 +1,25 @@
+<?php
+
+$num1 = 5 ;
+
+$num2 = 2 ;
+
+$resto = $num1 % $num2 ;
+$soma = $num1 + $num2 ;
+$subtracao = $num1 - $num2 ;
+$multiplicacao = $num1 * $num2 ;
+$divisao  = $num1 / $num2 ;
+$exponenciacao = $num1 ** $num2 ;
+
+echo "Soma $num1 + $num2 = $soma <br>" ;
+echo "Subtração $num1 - $num2 = $subtracao <br>" ;
+echo "Multiplicação $num1 x $num2 = $multiplicacao <br>" ;
+echo "Divisão $num1 / $num2 = $divisao <br> " ;
+echo "Resto da divisão $num1 % $num2 = $resto <br> ";
+echo "Exponenciação $num1 ** $num2 = $exponenciacao" ;
+
+
+
+
+
+
