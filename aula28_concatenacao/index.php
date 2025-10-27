2a684e4ff6e34b58bac3b7a6788fbcc6a786f58b
Magnus <rianmagnus5791@gmail.com>
2025-10-27 10:28:15 -0300

    concatenacao aula 28 e exercicio 17

---
+++ aula28_concatenacao/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Testando" . " a" . " concatenacao <br>" ;
+
+$testando = 'testando'; 
+$concatenacao = ' concatenação' ;
+
+$result = $testando . $concatenacao ;
+
+echo get_debug_type($concatenacao);
---
+++ exercicios/exercicio_17.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+$nome = 'Rian' ;
+
+$sobrenome = " Magnus" ;
+
+$nomeCompleto =  $nome . $sobrenome;
+
+echo "Eea $nomeCompleto !" ;
