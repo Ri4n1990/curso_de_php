974a566be332dccf5589ba719013b53c487fbcae
Magnus <rianmagnus5791@gmail.com>
2025-10-27 14:56:47 -0300

    operadores de cast aula 31 e exercicio 18

---
+++ aula31_operadores_de_cast/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$a = (int) '12';
+
+echo  get_debug_type($a);
+
+echo $a . "<br>" ;
+
+echo $a +10;
---
+++ exercicios/exercicio18.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$texto = (int) 'testando' ;
+$flutuante = (int) 12.9;
+$boleano = (int) true ;
+$lista = (int) [1,2,3] ;
+
+echo $texto . "<br>" ;
+echo $flutuante . "<br>" ;
+echo $boleano  . "<br>";
+echo $lista ;
