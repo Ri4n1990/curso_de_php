89fb95eb5ca26df24b54fad7e38b742f90069cb4
Magnus <rianmagnus5791@gmail.com>
2025-10-29 16:47:57 -0300

    estrutura de controle match aula 39

---
+++ aula39_match/index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$roupa = 'carro' ;
+
+$retorno = match($roupa){
+
+    'calça' => 'é uma calça' ,
+    'vestido' => 'é um vestido' ,
+    default => 'não é uma roupa',
+
+
+
+
+};
+
+
+
+echo $retorno . '<br>';
+
+
+
+
+
+
+$idade = 'oi' ;
+
+
+
+
+$resultado = match(true){
+
+    $idade < 18 => 'Menor de idade',
+    $idade >= 18 =>  'Maior de idade',
+    default =>  'Algo saiu errado',
+    
+
+};
+
+
+
+echo $resultado ;
