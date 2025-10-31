b1607b47036e4535c4066383f56ae29b9ebc5051
Magnus <rianmagnus5791@gmail.com>
2025-10-31 15:17:01 -0300

    return em funcoes aula 54 e exercicio 45

---
+++ aula54_return/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+function soma($n1 , $n2){
+    $soma = $n1 + $n2 ; 
+
+    return $soma ;
+}
+
+
+$val = soma(5,5) ;
+$calc = soma(10,15) ;
+echo $calc . "<br>" ;
+echo $val ;
+
---
+++ exercicios/exercicio45.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+function quadrado($num){
+
+    return $num * $num ;
+}
+
+
+
+$val =  quadrado(3);
+
+echo $val ;
