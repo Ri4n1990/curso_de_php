66cc3988de6a68d437f1e5d96aebed8b2c08a6e3
Magnus <rianmagnus5791@gmail.com>
2025-11-01 15:17:15 -0300

    strip tags aula 65 e exercicio 59

---
+++ aula65_strip_tags/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$textohtml = "<h1>Testando titulo</h1>" ;
+
+echo $textohtml ;
+
+$texto = strip_tags($textohtml) ;
+
+
+echo $texto ;
---
+++ exercicios/exercicio59.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$frase = "Cadê meu queijo? ele stava aqui." ;
+
+$queijo =   mb_substr($frase , 9 , 6 ) ; 
+
+echo $queijo;
