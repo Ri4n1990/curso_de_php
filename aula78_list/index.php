aae87ecddaec5760149e18be48498c00f55e0054
Magnus <rianmagnus5791@gmail.com>
2025-11-06 17:51:23 -0300

    função list aula 78 e exercicio 66

---
+++ aula78_list/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$pessoa = ["Matheus",29, "Programador", "Verdes"] ;
+
+list($nome , $idade , $profissao,$corolhos) = $pessoa ;
+
+
+print_r($pessoa) ;
+echo "<br>";
+
+echo "$nome<br>$idade<br>$profissao<br>$corolhos";
+
---
+++ exercicios/exercicio66.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+$carro = ['Jaguar' ,3.0 , "Azul" , 18 , "Teto solar automático"] ;
+
+
+list($marca , $potencia, $cor , $ano , $caract) = $carro ;
+
+echo "$marca<br>$potencia<br>$cor<br>$ano<br>$caract" ;
