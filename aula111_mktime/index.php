a224ee655514477f6890bb536eb621055a622e58
Magnus <rianmagnus5791@gmail.com>
2025-11-13 18:38:46 -0300

    aula 111 mktime

---
+++ aula111_mktime/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+
+$data_nascimento = mktime(12,00,00, 1,3,2004) ;
+
+echo date('d/m/Y', $data_nascimento). "<br>" ;
+
+
+$mae_aniversario = mktime(03,20,00,8,4,1994);
+
+echo date("d/m/Y", $mae_aniversario) ;
